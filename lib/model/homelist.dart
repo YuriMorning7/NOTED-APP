21a562711340062e1a43f516b77f32d52e95d1e4
John Lloyd <lloydeast1@gmail.com>
2023-10-26 17:38:59 +0800

    first commit

---
+++ lib/model/homelist.dart
@@ -0,0 +1,24 @@
+import 'package:best_flutter_ui_templates/fitness_app/fitness_app_home_screen.dart';
+import 'package:best_flutter_ui_templates/introduction_animation/introduction_animation_screen.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeList {
+  HomeList({
+    this.navigateScreen,
+    this.imagePath = '',
+  });
+
+  Widget? navigateScreen;
+  String imagePath;
+
+  static List<HomeList> homeList = [
+    HomeList(
+      imagePath: 'assets/introduction_animation/introduction_animation.png',
+      navigateScreen: IntroductionAnimationScreen(),
+    ),
+    HomeList(
+      imagePath: 'assets/fitness_app/fitness_app.png',
+      navigateScreen: FitnessAppHomeScreen(),
+    ),
+  ];
+}
